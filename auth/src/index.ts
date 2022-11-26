2da2ca518a83bbc0b6c9734c9955bc7944e695d8
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-11-26 16:01:31 +0200

    add empty error-handler middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { currentUserRouter } from "../routes/current-user";
 import { signinRouter } from "../routes/signin";
 import { signoutRouter } from "../routes/signout";
 import { signupRouter } from "../routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use("/api/users", [
   signupRouter,
   currentUserRouter,
 ]);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log(`Auth Service is Listening on Port 3000`);
