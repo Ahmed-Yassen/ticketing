c5bef102aa962e29d4ebabee4c0ba5ca791681de
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-12-03 17:31:23 +0200

    move middlewares and exceptions to the common module

--- auth/src/errors/custom-exception.ts
+++
@@ -1,9 +0,0 @@
-export abstract class CustomException extends Error {
-  constructor(exceptionMessage: string) {
-    super(exceptionMessage);
-    Object.setPrototypeOf(this, CustomException.prototype);
-  }
-
-  abstract statusCode: number;
-  abstract serializeErrors(): { message: string; field?: string }[];
-}
--- auth/src/errors/env-missing-exception.ts
+++
@@ -1,7 +0,0 @@
-export class EnvMissingException extends Error {
-  constructor(env: string) {
-    super(`ENV is Missing: ${env}`);
-
-    Object.setPrototypeOf(this, EnvMissingException.prototype);
-  }
-}
