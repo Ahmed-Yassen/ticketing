f34fca98a674548839189d96bf63b292d6ae7266
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-12-04 11:45:21 +0200

    add validation to create-ticket endpoint

--- tickets/src/routes/__test__/create-ticket.test.ts
+++ tickets/src/routes/__test__/create-ticket.test.ts
@@ -6,8 +6,20 @@ it("can only be accessed if the user is logged in", async () => {
   await request(app).post("/api/tickets/create").send({}).expect(401);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns a 400 if an invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets/create")
+    .set("Cookie", getCookieWithJwt())
+    .send({ title: "", price: 10 })
+    .expect(400);
+});
 
-it("returns an error if an invalid price is provided", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets/create")
+    .set("Cookie", getCookieWithJwt())
+    .send({ title: "Valid Title", price: -10 })
+    .expect(400);
+});
 
 it("creates a ticket with valid inputs", async () => {});
--- tickets/src/routes/__test__/create-ticket.ts
+++ tickets/src/routes/__test__/create-ticket.ts
@@ -1,14 +1,24 @@
-import { Router } from "express";
-import { requireAuth } from "@ayticketing/common";
+import { Request, Response, Router } from "express";
+import { requireAuth, validateRequest } from "@ayticketing/common";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/create", requireAuth, (req, res) => {
-  if (req.cookies.Authentication) {
-    console.log(req.cookies.Authentication);
-    return res.status(201).send();
-  }
-  res.status(500).send();
-});
+router.post(
+  "/create",
+  requireAuth,
+  [
+    body("title")
+      .not()
+      .isEmpty()
+      .isString()
+      .withMessage("Title should be valid"),
+    body("price")
+      .isFloat({ min: 1 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as createTicketRouter };
