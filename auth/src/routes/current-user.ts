d8b5730a8bfe9e89cf076da3dfa041ce35db17fa
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-12-04 15:06:45 +0200

    fix bug in current-user endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { requireAuth } from "@ayticketing/common";
+import { User } from "../models/user";
 const router = Router();
 
 router.get("/currentuser", requireAuth, async (req, res) => {
-  res.send({ currentUser: req.user });
+  const userId = req.userId;
+  const user = await User.findById(userId);
+  res.send({ currentUser: user });
 });
 
 export { router as currentUserRouter };
