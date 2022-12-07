aaee710fc96fc1723c8da9359b042eda4bd847d5
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-12-07 10:12:12 +0200

    init orders-service

---
+++ orders/src/index.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { EnvMissingException } from "@ayticketing/common";
+import app from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_SECRET) throw new EnvMissingException("JWT_SECRET");
+  if (!process.env.MONGO_URI) throw new EnvMissingException("MONGO_URI");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Orders Service Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Orders Service is Listening on Port 3000`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
