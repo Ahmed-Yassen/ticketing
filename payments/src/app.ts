2bda7a59fef6b10b8ff28e3be069380c20881aa9
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-12-09 13:27:21 +0200

    init payments-service

---
+++ payments/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import "express-async-errors";
+import { errorHandler, NotFoundException } from "@ayticketing/common";
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.all("*", () => {
+  throw new NotFoundException("URL");
+});
+
+app.use(errorHandler);
+
+export default app;
