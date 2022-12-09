69b07533057447f611dfc6f1efccf36da91f6fa5
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2022-12-09 15:33:59 +0200

    add validation and tests to create-charge endpoint in payments-service

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,36 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+let mongo: MongoMemoryServer;
+
+jest.mock("../../node_modules/@ayticketing/common/build/events/producer.js");
+
+beforeAll(async () => {
+  process.env.JWT_SECRET = "TestsJwtSecret";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+const getCookieWithJwt = (id?: mongoose.Types.ObjectId) => {
+  const token = jwt.sign(
+    { id: id || new mongoose.Types.ObjectId() },
+    process.env.JWT_SECRET!
+  );
+  return `Authentication=${token}; HttpOnly; Path=/;`;
+};
+
+export { getCookieWithJwt };
